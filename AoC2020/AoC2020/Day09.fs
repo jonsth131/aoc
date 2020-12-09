4ecf8415f8d5adf1489e4cbc437123aa74cb529b
Jonas <jonas1111@gmail.com>
2020-12-09 12:09:01 +0100

    Cleanup

--- AoC2020/AoC2020/Day09.fs
+++ AoC2020/AoC2020/Day09.fs
@@ -4,9 +4,11 @@ let input =
     Utils.readInputAsUint64 "Day09.txt"
     |> Array.toList
 
-let getSums (value: uint64) (preamble: uint64 list) = preamble |> Seq.map (fun x -> x + value)
+let getSums value preamble =
+    preamble
+    |> Seq.map (fun currValue -> currValue + value)
 
-let isValidValue (value: uint64) (preamble: uint64 list) =
+let isValidValue value preamble =
     preamble
     |> Seq.map (fun value ->
         getSums
@@ -16,22 +18,22 @@ let isValidValue (value: uint64) (preamble: uint64 list) =
     |> Seq.concat
     |> Seq.contains value
 
-let findInvalidElement (preambleLength: int) (input: uint64 list) =
+let findInvalidElement preambleLength (input: uint64 list) =
     input
     |> Seq.indexed
-    |> Seq.fold (fun result (i, x) ->
+    |> Seq.fold (fun result (i, value) ->
         if i < preambleLength then result
-        elif isValidValue x (input.[i - preambleLength - 1..i - 1]) then result
-        else x) 0UL
+        elif isValidValue value (input.[i - preambleLength - 1..i - 1]) then result
+        else value) 0UL
 
-let rec checkValue (value: uint64) first last (input: uint64 list) =
+let rec checkValue value first last (input: uint64 list) =
     let totalSum = input.[first..last] |> List.sum
     if totalSum = value then input.[first..last]
     elif totalSum > value then checkValue value (first + 1) (first + 2) input
     elif totalSum < value then checkValue value first (last + 1) input
     else []
 
-let findSumElements (input: uint64 list) (value: uint64) =
+let findSumElements input value =
     input |> checkValue value 0 1 |> List.sort
 
 let part1 = input |> findInvalidElement 25
