e1a2a35092531fd744f1843a8a769ed641570136
Jonas S <jonas1111@gmail.com>
2021-12-02 10:31:52 +0100

    Refactor tests

--- aoc2021/test_day1.py
+++ aoc2021/test_day1.py
@@ -1,11 +1,12 @@
 from day1 import part1, part2
 
 
+test_input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+
 def test_part1():
-    data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-    assert part1(data) == 7
+    assert part1(test_input) == 7
 
 
 def test_part2():
-    data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-    assert part2(data) == 5
+    assert part2(test_input) == 5
--- aoc2021/test_day2.py
+++ aoc2021/test_day2.py
@@ -1,11 +1,12 @@
 from day2 import part1, part2
 
 
+test_input = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
+
+
 def test_part1():
-    data = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
-    assert part1(data) == 150
+    assert part1(test_input) == 150
 
 
 def test_part2():
-    data = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
-    assert part2(data) == 900
+    assert part2(test_input) == 900
