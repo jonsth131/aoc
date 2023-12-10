de38a0489bedcc87f7a45a8ef52c0a64e3d1f46d
Jonas S <jonas1111@gmail.com>
2023-12-10 18:54:17 +0100

    Add day 10

---
+++ aoc2023/days/day10.ts
@@ -0,0 +1,171 @@
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+    None,
+}
+
+type Point = {
+    x: number;
+    y: number;
+};
+
+type Start = {
+    point: Point;
+    direction: Direction;
+};
+
+type Maze = {
+    grid: string[];
+    start: Start;
+    end: Point;
+};
+
+function part1(maze: Maze): number {
+    const seen = maze.grid.map((_) => _.split("").map((_) => false));
+    const path: Point[] = [];
+    walk(maze, maze.start.point, maze.start.direction, seen, path);
+    return path.length / 2;
+}
+
+function part2(maze: Maze): number {
+    const seen = maze.grid.map((_) => _.split("").map((_) => false));
+    const path: Point[] = [];
+    walk(maze, maze.start.point, maze.start.direction, seen, path);
+    const area = getTotalArea(maze.grid, path);
+    return area - (path.length / 2) + 1;
+}
+
+function getTotalArea(grid: string[], sections: Point[]): number {
+    const corners = sections.filter((pos) => grid[pos.y][pos.x] !== "." && grid[pos.y][pos.x] !== "|" && grid[pos.y][pos.x] !== "-");
+    let sum = 0;
+    for (let i = 1; i < corners.length; i++) {
+        const prev = corners[i - 1];
+        const curr = corners[i];
+        sum += (prev.y + curr.y) * (prev.x - curr.x);
+    }
+    sum += (corners[corners.length - 1].y + corners[0].y) * (corners[corners.length - 1].x - corners[0].x);
+    return Math.abs(sum) / 2;
+}
+
+function walk(maze: Maze, curr: Point, prevDirection: Direction, seen: boolean[][], path: Point[]): boolean {
+    if (curr.x < 0 || curr.x >= maze.grid[0].length || curr.y < 0 || curr.y >= maze.grid.length) {
+        return false;
+    }
+    if (seen[curr.y][curr.x]) {
+        return false;
+    }
+    if (curr.x === maze.end.x && curr.y === maze.end.y && prevDirection !== Direction.None) {
+        path.push(curr);
+        return true;
+    }
+
+    seen[curr.y][curr.x] = true;
+    path.push(curr);
+    const nextDirection = getNextDirection(maze, curr, prevDirection);
+    const nextPoint = getNextPoint(curr, nextDirection);
+
+    if (walk(maze, nextPoint, nextDirection, seen, path)) {
+        return true;
+    }
+
+    path.pop();
+
+    return true;
+}
+
+function getNextPoint(curr: Point, direction: Direction): Point {
+    switch (direction) {
+        case Direction.Up:
+            return { x: curr.x, y: curr.y - 1 };
+        case Direction.Right:
+            return { x: curr.x + 1, y: curr.y };
+        case Direction.Down:
+            return { x: curr.x, y: curr.y + 1 };
+        case Direction.Left:
+            return { x: curr.x - 1, y: curr.y };
+        default:
+            return curr;
+    }
+}
+
+function getNextDirection(maze: Maze, curr: Point, prevDirection: Direction): Direction {
+    const currTile = maze.grid[curr.y][curr.x];
+    switch (currTile) {
+        case "|":
+            return prevDirection === Direction.Up ? Direction.Up : Direction.Down;
+        case "-":
+            return prevDirection === Direction.Left ? Direction.Left : Direction.Right;
+        case "L":
+            return prevDirection === Direction.Left ? Direction.Up : Direction.Right;
+        case "J":
+            return prevDirection === Direction.Right ? Direction.Up : Direction.Left;
+        case "7":
+            return prevDirection === Direction.Right ? Direction.Down : Direction.Left;
+        case "F":
+            return prevDirection === Direction.Left ? Direction.Down : Direction.Right;
+        default:
+            return Direction.None;
+    }
+}
+
+function parseMaze(input: string): Maze {
+    const grid = input.split("\n");
+    const start = findStart(grid);
+    const end = findEnd(grid);
+    return { grid, start, end };
+}
+
+function findStart(grid: string[]): Start {
+    const end = findEnd(grid);
+    if (end.y > 0 && canWalk(grid[end.y - 1][end.x], Direction.Up)) {
+        return { direction: Direction.Up, point: { x: end.x, y: end.y - 1 } };
+    } else if (end.x < grid[0].length && canWalk(grid[end.y][end.x + 1], Direction.Right)) {
+        return { direction: Direction.Right, point: { x: end.x + 1, y: end.y } };
+    } else if (end.y < grid.length && canWalk(grid[end.y + 1][end.x], Direction.Down)) {
+        return { direction: Direction.Down, point: { x: end.x, y: end.y + 1 } };
+    } else if (end.x > 0 && canWalk(grid[end.y][end.x - 1], Direction.Left)) {
+        return { direction: Direction.Left, point: { x: end.x - 1, y: end.y } };
+    }
+    return { direction: Direction.None, point: { x: 0, y: 0 } };
+}
+
+function canWalk(tile: string, direction: Direction): boolean {
+    switch (tile) {
+        case "|":
+            return direction === Direction.Up || direction === Direction.Down;
+        case "-":
+            return direction === Direction.Left || direction === Direction.Right;
+        case "L":
+            return direction === Direction.Left || direction === Direction.Down;
+        case "J":
+            return direction === Direction.Right || direction === Direction.Down;
+        case "7":
+            return direction === Direction.Right || direction === Direction.Up;
+        case "F":
+            return direction === Direction.Left || direction === Direction.Up;
+        case "S":
+            return false;
+        default:
+            return false;
+    }
+}
+
+function findEnd(grid: string[]): Point {
+    for (let y = 0; y < grid.length; y++) {
+        const row = grid[y];
+        for (let x = 0; x < row.length; x++) {
+            if (row[x] === "S") {
+                return { x, y };
+            }
+        }
+    }
+    return { x: 0, y: 0 };
+}
+
+export function solve(input: string): [number, number] {
+    const maze = parseMaze(input);
+
+    return [part1(maze), part2(maze)];
+}
---
+++ aoc2023/tests/day10.test.ts
@@ -0,0 +1,68 @@
+import { test, expect, describe } from "bun:test";
+import { solve } from "../days/day10";
+
+describe("day10", () => {
+    test("part1 - simple", async () => {
+        const input = `.....
+.S-7.
+.|.|.
+.L-J.
+.....`;
+        const result = solve(input);
+        expect(result[0]).toBe(4);
+    });
+
+    test("part1 - complex", async () => {
+        const input = `..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...`;
+        const result = solve(input);
+        expect(result[0]).toBe(8);
+    });
+
+    test("part2 - easy", async () => {
+        const input = `...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........`;
+        const result = solve(input);
+        expect(result[1]).toBe(4);
+    });
+
+    test("part2 - complex", async () => {
+        const input = `.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...`;
+        const result = solve(input);
+        expect(result[1]).toBe(8);
+    });
+
+    test("part2 - full", async () => {
+        const input = `FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L`;
+        const result = solve(input);
+        expect(result[1]).toBe(10);
+    });
+});
