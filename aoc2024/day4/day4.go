6258f38ffdb0b9981b9f7fd881add08bad28eb0c
Jonas S <jonas1111@gmail.com>
2024-12-04 14:00:21 +0100

    Refactor day4

--- aoc2024/day4/day4.go
+++ aoc2024/day4/day4.go
@@ -13,10 +13,10 @@ const (
 	Down
 	Left
 	Right
-	Upleft
-	Upright
-	Downleft
-	Downright
+	DiagUpLeft
+	DiagUpRight
+	DiagDownLeft
+	DiagDownRight
 )
 
 func part1(input string) string {
@@ -26,27 +26,43 @@ func part1(input string) string {
 	res := 0
 	for y, row := range grid {
 		for x := range row {
-			res += getAdjacentTargets(grid, x, y, target)
+			for _, dir := range []Direction{Up, Down, Left, Right, DiagUpLeft, DiagUpRight, DiagDownLeft, DiagDownRight} {
+				if getAdjacentTarget(grid, x, y, target, dir) {
+					res++
+				}
+			}
 		}
 	}
 
 	return strconv.Itoa(res)
 }
 
-func getAdjacentTargets(grid [][]byte, x, y int, target string) int {
-	count := 0
-	for _, dir := range []Direction{Up, Down, Left, Right, Upleft, Upright, Downleft, Downright} {
-		if getAdjacentTarget(grid, x, y, target, dir) {
-			count++
+func part2(input string) string {
+	target := "MAS"
+	targetLen := len(target)
+	grid := util.ParseGrid(input)
+
+	res := 0
+	for y, row := range grid {
+		for x := range row {
+			if (getAdjacentTarget(grid, x, y, target, DiagDownRight) || getAdjacentTarget(grid, x+targetLen-1, y+targetLen-1, target, DiagUpLeft)) &&
+				(getAdjacentTarget(grid, x+targetLen-1, y, target, DiagDownLeft) || getAdjacentTarget(grid, x, y+targetLen-1, target, DiagUpRight)) {
+				res++
+			}
 		}
 	}
-	return count
+
+	return strconv.Itoa(res)
 }
 
 func getAdjacentTarget(grid [][]byte, x, y int, target string, dir Direction) bool {
 	targetLen := len(target)
 	height := len(grid)
 	width := len(grid[0])
+
+	if x < 0 || x >= width || y < 0 || y >= height {
+		return false
+	}
 
 	res := ""
 	switch dir {
@@ -78,28 +94,28 @@ func getAdjacentTarget(grid [][]byte, x, y int, target string, dir Direction) bo
 			}
 			return res == target
 		}
-	case Upleft:
+	case DiagUpLeft:
 		if x-targetLen+1 >= 0 && y-targetLen+1 >= 0 {
 			for i := 0; i < targetLen; i++ {
 				res += string(grid[y-i][x-i])
 			}
 			return res == target
 		}
-	case Upright:
+	case DiagUpRight:
 		if x+targetLen <= width && y-targetLen+1 >= 0 {
 			for i := 0; i < targetLen; i++ {
 				res += string(grid[y-i][x+i])
 			}
 			return res == target
 		}
-	case Downleft:
+	case DiagDownLeft:
 		if x-targetLen+1 >= 0 && y+targetLen <= height {
 			for i := 0; i < targetLen; i++ {
 				res += string(grid[y+i][x-i])
 			}
 			return res == target
 		}
-	case Downright:
+	case DiagDownRight:
 		if x+targetLen <= width && y+targetLen <= height {
 			for i := 0; i < targetLen; i++ {
 				res += string(grid[y+i][x+i])
@@ -110,36 +126,6 @@ func getAdjacentTarget(grid [][]byte, x, y int, target string, dir Direction) bo
 
 	return false
 }
-
-func part2(input string) string {
-	grid := util.ParseGrid(input)
-
-	res := 0
-	for y, row := range grid {
-		for x := range row {
-			if grid[y][x] == 'A' {
-				if isMatch(grid, x, y) {
-					res++
-				}
-			}
-		}
-	}
-
-	return strconv.Itoa(res)
-}
-
-func isMatch(grid [][]byte, x, y int) bool {
-	height := len(grid)
-	width := len(grid[0])
-
-	if y-1 >= 0 && y+1 < height && x-1 >= 0 && x+1 < width {
-		leftRightMatch := (grid[y-1][x-1] == 'M' && grid[y+1][x+1] == 'S') || (grid[y-1][x-1] == 'S' && grid[y+1][x+1] == 'M')
-		rightLeftMatch := (grid[y+1][x-1] == 'M' && grid[y-1][x+1] == 'S') || (grid[y+1][x-1] == 'S' && grid[y-1][x+1] == 'M')
-		return leftRightMatch && rightLeftMatch
-	}
-	return false
-}
-
 func Run(input string) string {
 	return part1(input) + ", " + part2(input)
 }
